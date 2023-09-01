0a21eb633ae96355ccabf9e3a6edc5cea90737e8
VictorYudi28 <vyudi.abe@gmail.com>
2023-09-01 16:03:16 -0300

    Colocando o projeto no github

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// const linksMenu = document.querySelectorAll('.nav-link');
+
+// linksMenu.forEach( (element)=> {
+
+//     element.addEventListener('click', (event)=> {
+//         menuActive(event.target)
+//     })
+
+// })
+
+// function menuActive(elementoClicado){
+
+//     elementoClicado.style.borderBottom = '2px solid #BE8845';
+
+//     console.log(elementoClicado)
+
+// }
+
