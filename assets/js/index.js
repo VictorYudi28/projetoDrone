d0cb06b6493fd349be8c73a9415fa923d55909ad
Victor Yudi Abe de Almeida <vyudi.abe@gmail.com>
2023-11-05 14:28:09 -0300

    Colocando o projeto no github

---
+++ assets/js/index.js
@@ -0,0 +1,79 @@
+// Objetos cards principal
+
+const objetosCardsIndex = [
+
+    {
+        imagem: "drone1.jpg",
+        altImagem: 'Imagem de um drone voando',
+        tituloH: "Lorem Ipsum",
+        textoP:
+            " Some quick example text to build on the card title and make up the bulk of the cards content.",
+        btnLink: "#",
+    },
+
+    {
+        imagem: "drone2.jpg",
+        altImagem: 'Imagem de um drone voando',
+        tituloH: "Lorem Ipsum",
+        textoP:
+            " Some quick example text to build on the card title and make up the bulk of the cards content.",
+        btnLink: "#",
+    },
+
+    {
+        imagem: "drone3.jpg",
+        altImagem: 'Imagem de um drone voando',
+        tituloH: "Lorem Ipsum",
+        textoP:
+            " Some quick example text to build on the card title and make up the bulk of the cards content.",
+        btnLink: "#",
+    },
+
+    {
+        imagem: "drone4.jpg",
+        altImagem: 'Imagem de um drone voando',
+        tituloH: "Lorem Ipsum",
+        textoP:
+            " Some quick example text to build on the card title and make up the bulk of the cards content.",
+        btnLink: "#",
+    },
+
+]
+
+const sectionCardsElement = document.querySelector('.section-cards-cont');
+
+objetosCardsIndex.forEach( objetoCard => {
+    
+    sectionCardsElement.innerHTML += `
+    
+    <div class="col-md-6 col-lg-3 p-2">
+        <div class="card">
+            <img src="./assets/images/imagesIndex/${objetoCard.imagem}" class="card-img-top" alt="${objetoCard.altImagem}">
+            <div class="card-body">
+                <h5 class="card-title">${objetoCard.tituloH}</h5>
+                <p class="card-text">
+                    ${objetoCard.textoP}
+                </p>
+                <a href="${objetoCard.btnLink}" class="btn btn-primary">Saiba mais</a>
+            </div>
+        </div>
+    </div>
+
+    `
+
+});
+
+const cards = sectionCardsElement.querySelectorAll('.card')
+
+cards.forEach((card) => {
+
+    card.addEventListener('mouseover' , ()=> {
+
+        card.classList.add('scale')
+
+    })
+
+})
+
+
+
---
+++ assets/js/service.js
@@ -0,0 +1,69 @@
+// Objetos serviços
+
+const objetosCardsServiços = [
+
+    {
+        imagem: "painelSolar.jpg",
+        altImagem: 'Imagem de um homem analisando um painel solar',
+        tituloH: "Painel Solar",
+        textoP:
+            " Some quick example text to build on the card title and make up the bulk of the cards content.",
+        btnLink: "#",
+    },
+
+    {
+        imagem: "fotografiaDrone.jpg",
+        altImagem: 'Imagem de um drone com seus equipamentos',
+        tituloH: "Fotografia Aérea",
+        textoP:
+            " Some quick example text to build on the card title and make up the bulk of the cards content.",
+        btnLink: "#",
+    },
+
+    {
+        imagem: "homemPilotandoDrone.jpg",
+        altImagem: 'Imagem de um homem pilotando um drone',
+        tituloH: "Filmagem",
+        textoP:
+            " Some quick example text to build on the card title and make up the bulk of the cards content.",
+        btnLink: "#",
+    },    
+
+    
+
+];
+
+const serviceCont = document.querySelector(".service-cont");
+
+objetosCardsServiços.forEach((objetoCard) => {
+
+    serviceCont.innerHTML += `
+    
+    <div class=" col-lg-4 my-2">
+        <div class="card">
+            <img src="../images/imagesService/${objetoCard.imagem}" class="card-img-top img-fluid" alt="${objetoCard.altImagem}" />
+            <div class="card-body">
+                <h5 class="card-title">${objetoCard.tituloH}</h5>
+                <p class="card-text">
+                    ${objetoCard.textoP}
+                </p>
+                <a href="${objetoCard.btnLink}" class="btn btn-primary">Saiba Mais</a>
+            </div>
+        </div>
+    </div>
+    
+    `;
+
+});
+
+const cards = serviceCont.querySelectorAll('.card')
+
+cards.forEach((card) => {
+
+    card.addEventListener('mouseover' , ()=> {
+
+        card.classList.add('scale')
+
+    })
+
+})
